3eb6f8a80405f650979c5b68df70e9014c191c8c
tibbi <tibor@kaputa.sk>
2016-08-14 18:28:29 +0200

    remove gson

--- app/src/main/kotlin/com/simplemobiletools/applauncher/activities/LicenseActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/applauncher/activities/LicenseActivity.kt
@@ -14,9 +14,5 @@ class LicenseActivity : SimpleActivity() {
         license_kotlin_title.setOnClickListener {
             startActivity(viewIntent(getString(R.string.kotlin_url)))
         }
-
-        license_gson_title.setOnClickListener {
-            startActivity(viewIntent(getString(R.string.gson_url)))
-        }
     }
 }
