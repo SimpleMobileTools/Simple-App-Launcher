cf0405b9c5b75f77b1ff58cd01768bf47e86d796
tibbi <tibor@kaputa.sk>
2016-08-14 12:48:12 +0200

    compare AppLaunchers by package name

--- app/src/main/kotlin/com/simplemobiletools/applauncher/models/AppLauncher.kt
+++ app/src/main/kotlin/com/simplemobiletools/applauncher/models/AppLauncher.kt
@@ -1,3 +1,7 @@
 package com.simplemobiletools.applauncher.models
 
-data class AppLauncher(val name: String, val pkgName: String, val iconId: Int)
+data class AppLauncher(val name: String, val pkgName: String, val iconId: Int) {
+    override fun equals(other: Any?): Boolean {
+        return pkgName.equals((other as AppLauncher).pkgName)
+    }
+}
