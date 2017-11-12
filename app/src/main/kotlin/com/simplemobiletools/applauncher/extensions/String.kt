c3f1dad0aacfa5d911b01d16b71e40b67a44eb40
tibbi <tibor@kaputa.sk>
2017-11-12 15:16:46 +0100

    removing a comment

--- app/src/main/kotlin/com/simplemobiletools/applauncher/extensions/String.kt
+++ app/src/main/kotlin/com/simplemobiletools/applauncher/extensions/String.kt
@@ -2,5 +2,4 @@ package com.simplemobiletools.applauncher.extensions
 
 import com.simplemobiletools.applauncher.helpers.predefinedPackageNames
 
-// treat apps like "com.simplemobiletools.notes.debug" as any third party app
 fun String.isAPredefinedApp() = predefinedPackageNames.contains(this)
