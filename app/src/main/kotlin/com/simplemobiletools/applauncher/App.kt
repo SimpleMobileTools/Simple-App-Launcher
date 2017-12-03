3f88043e908f909f277a7e7454efd19b4bc04d9b
tibbi <tibor@kaputa.sk>
2017-12-03 20:57:39 +0100

    initialize Stetho only in debug build

--- app/src/main/kotlin/com/simplemobiletools/applauncher/App.kt
+++ app/src/main/kotlin/com/simplemobiletools/applauncher/App.kt
@@ -7,7 +7,9 @@ import com.simplemobiletools.commons.extensions.checkUseEnglish
 class App : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Stetho.initializeWithDefaults(this)
+        }
         checkUseEnglish()
-        Stetho.initializeWithDefaults(this)
     }
 }
