ab782a00a90a684d14ea29b18a087b2d8de935da
tibbi <tibor@kaputa.sk>
2021-11-07 21:28:03 +0100

    adding some setting UI changes

--- app/src/main/kotlin/com/simplemobiletools/applauncher/activities/SettingsActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/applauncher/activities/SettingsActivity.kt
@@ -23,6 +23,14 @@ class SettingsActivity : SimpleActivity() {
         setupUseEnglish()
         setupCloseApp()
         updateTextColors(settings_holder)
+
+        arrayOf(settings_color_customization_label, settings_general_settings_label).forEach {
+            it.setTextColor(getAdjustedPrimaryColor())
+        }
+
+        arrayOf(settings_color_customization_holder, settings_general_settings_holder).forEach {
+            it.background.applyColorFilter(baseConfig.backgroundColor.getContrastColor())
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -32,6 +40,12 @@ class SettingsActivity : SimpleActivity() {
 
     private fun setupPurchaseThankYou() {
         settings_purchase_thank_you_holder.beGoneIf(isOrWasThankYouInstalled())
+
+        // make sure the corners at ripple fit the stroke rounded corners
+        if (settings_purchase_thank_you_holder.isGone()) {
+            settings_use_english_holder.background = resources.getDrawable(R.drawable.ripple_top_corners, theme)
+        }
+
         settings_purchase_thank_you_holder.setOnClickListener {
             launchPurchaseThankYouIntent()
         }
@@ -47,6 +61,11 @@ class SettingsActivity : SimpleActivity() {
     private fun setupUseEnglish() {
         settings_use_english_holder.beVisibleIf(config.wasUseEnglishToggled || Locale.getDefault().language != "en")
         settings_use_english.isChecked = config.useEnglish
+
+        if (settings_use_english_holder.isGone() && settings_purchase_thank_you_holder.isGone()) {
+            settings_close_app_holder.background = resources.getDrawable(R.drawable.ripple_all_corners, theme)
+        }
+
         settings_use_english_holder.setOnClickListener {
             settings_use_english.toggle()
             config.useEnglish = settings_use_english.isChecked
